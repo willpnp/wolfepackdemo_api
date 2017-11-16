dcf850c92a7f3a1c3d3acb72ae14afcb3ff54951
Will Decker <will.decker@pnptc.com>
2017-11-15 21:26:53 -0800

    Cleaned up code

--- routes/loginroutes.js
+++ routes/loginroutes.js
@@ -2,99 +2,90 @@ var mysql = require('mysql');
 var bcrypt = require('bcrypt');
 var connection;
 
-if (process.env.NODE_ENV == 'development'){
-	connection = mysql.createConnection({
-	  host     : process.env.DB_HOST,
-	  user     : process.env.DB_USER,
-	  password : process.env.DB_PASS,
-	  database : process.env.DB_DB,
-	});
-} else {
-	connection = mysql.createConnection({
-	  host     : process.env.DB_HOST,
-	  user     : process.env.DB_USER,
-	  password : process.env.DB_PASS,
-	  database : process.env.DB_DB,
-	  port     : process.env.DB_PORT
-	});
+var connectionParams = {
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASS,
+  database : process.env.DB_DB
 }
 
-connection.connect(function(err){
-if(!err) {
-    console.log("Database is connected ... nn");
-} else {
-    console.log("Error connecting database ... nn");
+if (process.env.NODE_ENV != 'development'){
+	connectionParams[port] = process.env.DB_PORT
 }
+
+connection = mysql.createConnection(connectionParams);
+connection.connect(function(err){
+  if(!err) {
+    console.log("Database is connected ... nn");
+  } else {
+    console.log("Error connecting database ... nn");
+  }
 });
 
 exports.register = function(req,res){
   // console.log("req",req.body);
   var today = new Date();
 
-
   bcrypt.hash(req.body.password, 5, function( err, bcryptedPassword) {
    
-     var users={
-       "first_name":req.body.first_name,
-       "last_name":req.body.last_name,
-       "email":req.body.email,
-       "password":bcryptedPassword,
-       "created":today,
-       "modified":today
-     }
-	 console.log('we made it to the register step');
-  connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
-  if (error) {
-    console.log("error ocurred",error);
-    res.send({
-      "code":400,
-      "failed":"error ocurred"
-    })
-  } else {
-    console.log('The solution is: ', results);
-    res.send({
-      "code":200,
-      "success":"user registered sucessfully"
+    var users={
+      "first_name":req.body.first_name,
+      "last_name":req.body.last_name,
+      "email":req.body.email,
+      "password":bcryptedPassword,
+      "created":today,
+      "modified":today
+    }
+    connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
+      if (error) {
+        console.log("error ocurred",error);
+        res.send({
+          "code":400,
+          "failed":"error ocurred"
+        })
+      } else {
+        console.log('The solution is: ', results);
+        res.send({
+          "code":200,
+          "success":"user registered sucessfully"
         });
-  }
-  });
-})
+      }
+    });
+  })
 };
-
 
 exports.login = function(req,res){
   var email= req.body.email;
   var password = req.body.password;
   connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
-  if (error) {
-    // console.log("error ocurred",error);
-    res.send({
-      "code":400,
-      "failed":"error ocurred"
-    })
-  }else{
-    // console.log('The solution is: ', results);
-    if(results.length >0){
-      bcrypt.compare(password, results[0].password, function(err, doesMatch){
-        if (doesMatch){
-     res.send({
-       "code":200,
-       "success":"login sucessfull"
-         });
-      }else{
-     res.send({
-       "code":204,
-       "success":"Email and password does not match"
-         });
-      }
-    });
-    }
-    else{
+    if (error) {
+      // console.log("error ocurred",error);
       res.send({
-        "code":204,
-        "success":"Email does not exits"
-          });
+        "code":400,
+        "failed":"error ocurred"
+      })
+    } else {
+      // console.log('The solution is: ', results);
+      if(results.length >0){
+        bcrypt.compare(password, results[0].password, function(err, doesMatch){
+          if (doesMatch){
+            res.send({
+            "code":200,
+            "success":"login sucessfull"
+            });
+          } else {
+            res.send({
+            "code":204,
+            "success":"Email and password does not match"
+            });
+          }
+        });
+      } else {
+        res.send({
+          "code":204,
+          "success":"Email does not exits"
+        });
+      }
     }
-  }
   });
 };
